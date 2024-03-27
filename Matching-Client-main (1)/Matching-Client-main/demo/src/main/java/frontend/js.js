1a3270956c0e5c725ad82c19c6fb977b1f0e1f87
ProjectFar <F.E55@outlook.de>
2024-03-27 09:55:39 +0100

    aktuelle Version von html + Jquery

--- Matching-Client-main (1)/Matching-Client-main/demo/src/main/java/frontend/js.js
+++ Matching-Client-main (1)/Matching-Client-main/demo/src/main/java/frontend/js.js
@@ -1,13 +1,5 @@
-<html>
-<body>
-
-<p id="demo">
-
-</p>
-
-<script>
-  document.getElementById("A").innerHTML = "Hello World!";
-</script>
-
-</body>
-</html>
+function generateRandomNumber() {
+  var randomNumber = Math.floor(Math.random() * 100) + 1; // Zufällige Zahl zwischen 1 und 100 generieren
+  document.getElementById("numberBox").innerText = randomNumber; // Zufällige Zahl in den Kasten einfügen
+}
+//generateRandomNumber();
