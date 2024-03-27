64e190a34df3f0cdc111b2b0f9b7e478b920e5e9
ProjectFar <F.E55@outlook.de>
2024-03-27 10:56:34 +0100

    aktuelle Version von html + Jquery

--- Matching-Client-main (1)/Matching-Client-main/demo/src/main/java/frontend/js.js
+++ Matching-Client-main (1)/Matching-Client-main/demo/src/main/java/frontend/js.js
@@ -1,5 +1,5 @@
 function generateRandomNumber() {
   var randomNumber = Math.floor(Math.random() * 100) + 1; // Zufällige Zahl zwischen 1 und 100 generieren
-  document.getElementById("numberBox").innerText = randomNumber; // Zufällige Zahl in den Kasten einfügen
+  document.getElementById("idinputmatch").value = randomNumber; // Zufällige Zahl in den Kasten einfügen
 }
-//generateRandomNumber();
+generateRandomNumber();
