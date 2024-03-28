13667b81ad08bb16a791df0c120415d7ea279d6b
ProjectFar <F.E55@outlook.de>
2024-03-28 08:20:28 +0100

    Anmelde Felder angelegt funktion für zufälligen kollegen + reinschreiben in MatchID

--- Matching-Client-main (1)/Matching-Client-main/demo/src/main/java/frontend/js.js
+++ Matching-Client-main (1)/Matching-Client-main/demo/src/main/java/frontend/js.js
@@ -3,3 +3,47 @@ function generateRandomNumber() {
   document.getElementById("idinputmatch").value = randomNumber; // Zufällige Zahl in den Kasten einfügen
 }
 generateRandomNumber();
+
+
+function submitForms() {
+  document.getElementById("id").submit();
+  document.getElementById("restaurant").submit('');
+}
+
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x === "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+
+function submitFormsajax() {
+  const url = 'http://localhost:63342/';
+  const dataType = 'json';
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: 'data543453',
+    // success: success,
+    dataType: dataType
+  });
+}
+function alert() {
+  alert("So geht es");
+}
+function zeigeAlert() {
+  alert("Button wurde geklickt!");
+}
+$(document).ready(function(){
+  $("#submitbutton").click(function(){
+    alert("Vorname wurde gespeichert");
+  });
+});
+
+function generateRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  document.getElementById("idinputmatch").value = randomNumber;
+}
+
+generateRandomNumber();
