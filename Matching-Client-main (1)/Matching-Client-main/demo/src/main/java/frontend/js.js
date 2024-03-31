d566ba5e52757e3854c869734b6e361265565945
IanHanna <Ian.hanna@mail.schwarz>
2024-03-31 13:10:50 +0200

    minor bugfixes

--- Matching-Client-main (1)/Matching-Client-main/demo/src/main/java/frontend/js.js
+++ Matching-Client-main (1)/Matching-Client-main/demo/src/main/java/frontend/js.js
@@ -59,7 +59,10 @@ function findmatchajax() {
               // Set the text content of the "matchedUser" paragraph to the names of all matched users
               document.getElementById('matchedUser').textContent = matchedUserNames;
 
-              // Set the text content of the "matchedUserResult" paragraph to the entire result
+
+              if (uniqueUsers.length > 0) {
+                  document.getElementById('matchedUserResult').textContent = JSON.stringify(uniqueUsers, null, 2);
+              }
               document.getElementById('matchedUserResult').textContent = JSON.stringify(uniqueUsers, null, 2);
           } else {
               alert('No users found with the entered time');
@@ -72,42 +75,48 @@ function findmatchajax() {
 }
 
 function findRandomMatchAjax() {
-  var url = 'http://localhost:8080/matchusersRandomly';
+   var url = 'http://localhost:8080/matchusersRandomly';
 
-  $.ajax({
-      type: "GET",
-      url: url,
-      contentType: 'application/json',
-      success: function(response) {
-          console.log(response);
-          if(response.length > 0) {
-              // Filter out duplicate IDs
-              var uniqueUsers = [];
-              response.forEach(user => {
-                  if (!uniqueUsers.find(u => u.ID === user.ID)) {
-                      uniqueUsers.push(user);
-                  }
-              });
+   $.ajax({
+       type: "GET",
+       url: url,
+       contentType: 'application/json',
+       success: function(response) {
+           console.log(response);
+           if(response.length > 0) {
+               // Filter out duplicate IDs
+               var uniqueUsers = [];
+               response.forEach(user => {
+               //u == user
+                   if (!uniqueUsers.find(u => u.ID === user.ID)) {
+                       uniqueUsers.push(user);
+                   }
+               });
 
-              var matchedUserNames = uniqueUsers.map(user => user.name).join(', ');
+               // Get the names of all unique users
+               var matchedUserNames = uniqueUsers.map(user => user.name);
 
-              // Set the value of the "randomMatchedUserInput" textbox to the names of all matched users
-              document.getElementById('randomMatchedUserInput').value = matchedUserNames;
+               // Set the value of the "randomMatchedUserInput" textbox to the first user's name
+               if(matchedUserNames.length > 0) {
+                   document.getElementById('randomMatchedUserInput').value = matchedUserNames[0];
+               }
 
-              // Set the text content of the "randomMatchedUserResult" paragraph to the entire result
-              document.getElementById('randomMatchedUserResult').textContent = JSON.stringify(uniqueUsers, null, 2);
-          } else {
-              alert('No users found');
-          }
-      },
-      error: function(error) {
-          console.error(error);
-      }
-  });
-}
+               // Set the text content of the "randomMatchedUserResult" paragraph to the first user
+               if(uniqueUsers.length > 0) {
+                   document.getElementById('randomMatchedUserResult').textContent = JSON.stringify(uniqueUsers[0], 2);
+               }
+           } else {
+               alert('No users found');
+           }
+       },
+       error: function(error) {
+           console.error(error);
+       }
+   });
+ }
 
 // Bind the functions to the onclick events of the buttons
 document.getElementById('matchUsers').onclick = findmatchajax;
 
-//fix duped users. //a lot of users are shown twice
+//fix duped users.
 document.getElementById('matchUsersrandomly').onclick = findRandomMatchAjax;
