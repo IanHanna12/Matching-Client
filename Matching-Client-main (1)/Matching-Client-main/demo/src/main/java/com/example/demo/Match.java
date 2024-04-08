2c3aa1868ebd2c094279a1be84548b4ceca4f98c
IanHanna <Ian.hanna@mail.schwarz>
2024-04-08 09:35:21 +0200

    fixes

--- Matching-Client-main (1)/Matching-Client-main/demo/src/main/java/com/example/demo/Match.java
+++ Matching-Client-main (1)/Matching-Client-main/demo/src/main/java/com/example/demo/Match.java
@@ -6,21 +6,6 @@ public class Match {
         private User matchedUser;
 
 
-        public String getInitiator() {
-            return initiator;
-        }
-
-        public void setInitiator(String initiator) {
-            this.initiator = initiator;
-        }
-
-        public User getMatchedUser() {
-            return matchedUser;
-        }
-
-        public void setMatchedUser(User matchedUser) {
-            this.matchedUser = matchedUser;
-        }
 
         public Match(String initiator, User matchedUser) {
             this.initiator = initiator;
